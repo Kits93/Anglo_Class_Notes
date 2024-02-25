585b578f2d098663a6c6cba0fec7edef0277d381
Kits93 <Kiritsune14@gmail.com>
2024-02-24 22:52:41 -0300

    mduança de searchbar para select-option

--- ClassNote/src/app/pages/turma/turma.page.ts
+++ ClassNote/src/app/pages/turma/turma.page.ts
@@ -14,46 +14,32 @@ export class TurmaPage implements OnInit {
   constructor(private router: Router, private turmasService: TurmaService) { }
 
   ngOnInit() {
+    this.filtrarTurmas()
     this.listar_turmas()
-
   }
 
-  results: any[] = []
+  turmasFilter: any[] = []
 
+  ensinoSelected: any = ''
 
-  handleInput(event: any) {
-    const query = event.target.value.toLowerCase();
-    console.log(this.results)
-    console.log(query)
-    this.results = this.turmas.filter((turma: any) => turma.nome_turma.toLowerCase().includes(query));
+  filtrarTurmas(){
+    if (this.ensinoSelected === '') {
+      this.turmasFilter = this.turmas
+    } else {
+      this.turmasFilter = this.turmas.filter(turma => turma.ensino === this.ensinoSelected);
+    }
   }
-
 
   listar_turmas() {
     this.turmasService.read().subscribe((dados: any) => {
       console.log(dados)
       this.turmas = dados.turmas
-      this.results = this.turmas
+      this.turmasFilter = this.turmas
       if (!dados.success || dados.success != 1) {
         this.turmas = []
       }
     })
   }
-
-  items = [
-    { "turma": "3º ano D", "ensino": "Ensino Fundamental I" },
-    { "turma": "4º ano A", "ensino": "Ensino Fundamental I" },
-    { "turma": "7º ano B", "ensino": "Ensino Fundamental II" },
-    { "turma": "1º ano A", "ensino": "Ensino Médio" },
-    { "turma": "2º ano B", "ensino": "Ensino Médio" },
-    { "turma": "3º ano C", "ensino": "Ensino Médio" },
-    { "turma": "1º ano B", "ensino": "Ensino Fundamental I" },
-    { "turma": "2º ano A", "ensino": "Ensino Fundamental II" },
-    { "turma": "4º ano B", "ensino": "Ensino Fundamental I" },
-    { "turma": "5º ano C", "ensino": "Ensino Fundamental I" },
-    { "turma": "6º ano A", "ensino": "Ensino Fundamental II" },
-    { "turma": "9º ano D", "ensino": "Ensino Fundamental II" }
-  ]
 
   acessarTurma(turma: any) {
     console.log(turma);
